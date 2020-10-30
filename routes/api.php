9bb51117b91c05ff81f299373e1255331841f0c4
vagrant <vagrant@homestead>
2020-10-30 19:04:36 +0000

    Adds NoticeController in routes api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\NoticeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['middleware' =>'auth:api'], function(){
+    // Notice controller
+    Route::apiResource('notice', NoticeController::class);
+
 });
